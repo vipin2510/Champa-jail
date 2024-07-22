1a20e75872b372dbdfe523b5eb5af874d9a24956
vipin kumar gautam <vipinkumargautam@vipins.local>
2024-07-22 19:51:19 +0530

    updates

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ if not ON_VERCEL:
     from dotenv import load_dotenv
     load_dotenv()
 
-
 # Set up credentials using environment variables
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 credentials = {
@@ -33,22 +32,21 @@ creds = ServiceAccountCredentials.from_json_keyfile_dict(credentials, scope)
 client = gspread.authorize(creds)
 
 app = Flask(__name__)
-
-last_modified_time = None
+last_fetch_time = None
 
 @app.route('/', methods=['GET', 'POST'])
 def display_sheet():
-    global last_modified_time
+    global last_fetch_time
     try:
         # Open the spreadsheet
         sheet = client.open_by_key('1EWc4ZEPuE3oLvHmuQ5_4Bb7uR3Gy8bwYlRxdbuUf_4s').worksheet('Sheet1')
         
-        # Update the last modified time
-        last_modified_time = datetime.now()
+        # Update the last fetch time
+        last_fetch_time = datetime.now()
         
         # Get all values
         rows = sheet.get_all_values()
-        headers = rows[0][1:5]  # Get headers excluding the first column (क्रमांक) and last column (दिनांक)
+        headers = ['क्रमांक'] + rows[0][1:5]  # Exclude the first and last columns
         
         # Get the selected date or use today's date
         selected_date = request.form.get('selected_date', datetime.now().strftime("%Y-%m-%d"))
@@ -60,35 +58,36 @@ def display_sheet():
             try:
                 row_date = datetime.strptime(row[-1], "%m/%d/%Y")
                 if row_date.date() == selected_date_obj.date():
-                    filtered_data.append(row[1:5])  # Exclude first and last columns
+                    filtered_data.append(row[1:5])  # Exclude the first and last columns
             except ValueError:
+                app.logger.warning(f"Invalid date format in row: {row}")
                 continue  # Skip rows with invalid date format
         
         # Add serial numbers starting from 1 for each day
         filtered_data = [[i+1] + row for i, row in enumerate(filtered_data)]
         
         # Get current date and time
-        current_time = last_modified_time.strftime("%H:%M")
+        fetch_time = last_fetch_time.strftime("%H:%M:%S")
         
         # Check if data exists for the selected date
         if not filtered_data:
             error_message = f"Data not found for {selected_date_obj.strftime('%d/%m/%Y')}"
-            return render_template('sheet.html', headers=headers, data=[], current_time=current_time, 
+            return render_template('sheet.html', headers=headers, data=[], fetch_time=fetch_time,
                                    error_message=error_message, selected_date=selected_date)
         
-        return render_template('sheet.html', headers=['क्रमांक'] + headers, data=filtered_data, 
-                               current_time=current_time, selected_date=selected_date)
+        return render_template('sheet.html', headers=headers, data=filtered_data,
+                               fetch_time=fetch_time, selected_date=selected_date)
     except Exception as e:
+        app.logger.error(f"An error occurred: {str(e)}")
         return f"An error occurred: {str(e)}"
 
 @app.route('/check_changes')
 def check_changes():
-    global last_modified_time
+    global last_fetch_time
     sheet = client.open_by_key('1EWc4ZEPuE3oLvHmuQ5_4Bb7uR3Gy8bwYlRxdbuUf_4s').worksheet('Sheet1')
     current_modified_time = sheet.updated
-    
-    if current_modified_time != last_modified_time:
-        last_modified_time = current_modified_time
+    if current_modified_time != last_fetch_time:
+        last_fetch_time = current_modified_time
         return jsonify({"hasChanges": True})
     return jsonify({"hasChanges": False})
 
